ae418efa9a3cb5d6b4cf3b5081175844acecaac7
qbxl400 <63483381+qbxl400@users.noreply.github.com>
2022-04-23 15:22:38 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello AWS World!")
+from fastapi import FastAPI, status
+
+app= FastAPI()
+
+@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+def perofrm_healthcheck():
+        return {'healthcheck': 'Everthing OK!'}
